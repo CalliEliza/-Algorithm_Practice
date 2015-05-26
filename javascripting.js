b994b78eed37c7ae7ab279d03ad439cedb9f8013
Callie Brooks <calidesigns42@gmail.com>
2015-05-26 13:24:57 -0500

    Update javascripting.js

--- javascripting.js
+++ javascripting.js
@@ -1,12 +1,19 @@
 /**
  * Created by callie on 5/25/15.
  */
+ // array filtering function by even #
 var numbers = [1,2,3,4,5,6,7,8,9,10];
+var even = [];
+var filtered = function(array) {
+    for (i = 0; i < array.length; ++i) {
+        if (array[i] % 2 === 0) {
+            even.push(array[i]);
+        }
+    }
+    return even;
+};
 
-var filtered = numbers.filter(function(number) {
-    return (number % 2 === 0);
-});
 
-var fil = numbers.filter();
 
-console.log(fil);
+filtered(numbers);
+
