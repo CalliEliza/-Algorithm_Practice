6e042c1cc9bc0dc58f6b1065e797db5382383329
Callie Brooks <calidesigns42@gmail.com>
2015-05-26 12:50:43 -0500

    Update FCCpractice.js
    added function

--- FCCpractice.js
+++ FCCpractice.js
@@ -1,6 +1,8 @@
 /**
  * Created by callie on 5/26/15.
  */
+ 
+ // reverse a string
 function reverseString(str) {
     var strArray = str.split("");
     var ArrayReverse = strArray.reverse();
@@ -8,3 +10,14 @@ function reverseString(str) {
 };
 
 console.log(reverseString('hello'));
+
+// factorial function
+function factorialize(num) {
+    var result = num;
+    for(i=1;i<num; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+factorialize(5);
