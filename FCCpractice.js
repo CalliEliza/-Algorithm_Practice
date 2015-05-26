1a2e27bb5cc6aadb47b35a2058fc6c445b57329c
Callie Brooks <calidesigns42@gmail.com>
2015-05-26 15:01:02 -0500

    Update FCCpractice.js

--- FCCpractice.js
+++ FCCpractice.js
@@ -21,3 +21,17 @@ function factorialize(num) {
 }
 
 factorialize(5);
+
+// palindromes
+
+function plaindrome(str) {
+    var lwrstr = str.toLowerCase();  // ensures all letters on lower case
+    var str_Arr = lwrstr.split("");  //turns string into array
+
+    if (str_Arr[0] === str_Arr[str_Arr.length-1]) {  // checks the first and last value to see if equal
+        return true;
+    }
+    else {
+        return false;
+    }
+};
