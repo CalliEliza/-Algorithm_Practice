b6c993419ce790b7522ed11431e0386e4ea3e7d1
Callie Brooks <calidesigns42@gmail.com>
2015-06-05 19:55:35 -0500

    Update JavaBookPP.js

--- JavaBookPP.js
+++ JavaBookPP.js
@@ -13,3 +13,21 @@ function DrainAccount() {
     }
     return monthsToZero;
 }
+
+// 1.9 //
+function DrainAct(account, interest, expense) {
+    var monthsToZero = 0;
+    var yearsToZero = 0;
+    if ((account <= 0) || (account < expense) || account < interest) {
+        return console.log("Please check the information you entered. A value seems to be invalid");
+    }
+    else {
+        while (account > 0) {
+            account = account - (account * interest);
+            account = account - expense;
+            monthsToZero++;
+        }
+        yearsToZero = monthsToZero / 12;
+        return yearsToZero;
+    }
+}
